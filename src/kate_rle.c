6c9c0b30dc0dae6e3f2af4e241d7792502d2da4e
ogg.k.ogg.k <ogg.k.ogg.k@googlemail.com>
2008-05-25 16:37:00 +0000

    delta/stop encoding, and store a global zero per bitmap

--- src/kate_rle.c
+++ src/kate_rle.c
@@ -28,12 +28,15 @@
 #define KATE_RLE_RUN_LENGTH_BITS_BASIC_STARTEND_START 9
 #define KATE_RLE_RUN_LENGTH_BITS_BASIC_STARTEND_END 8
 #define KATE_RLE_RUN_LENGTH_BITS_BASIC_STOP_START 8
+#define KATE_RLE_RUN_LENGTH_BITS_BASIC_IN_DELTA_STOP 3
+#define KATE_RLE_RUN_LENGTH_BITS_DELTA_STOP 6
 
 #define KATE_RLE_TYPE_EMPTY 0
 #define KATE_RLE_TYPE_BASIC 1
 #define KATE_RLE_TYPE_DELTA 2
 #define KATE_RLE_TYPE_BASIC_STOP 3
 #define KATE_RLE_TYPE_BASIC_STARTEND 4
+#define KATE_RLE_TYPE_DELTA_STOP 5
 
 #define KATE_RLE_TYPE_BITS 3
 
@@ -75,7 +78,7 @@ static size_t get_run_length_zero_back(size_t max_run_length,size_t count,const 
   return run_length;
 }
 
-static int kate_rle_encode_line_basic(size_t count,const unsigned char *values,size_t bits,const unsigned char *previous kate_unused,kate_pack_buffer *kpb)
+static int kate_rle_encode_line_basic(size_t count,const unsigned char *values,size_t bits,unsigned char zero kate_unused,const unsigned char *previous kate_unused,kate_pack_buffer *kpb)
 {
   const size_t run_length_bits=KATE_RLE_RUN_LENGTH_BITS_BASIC;
   const size_t run_length_cutoff=1<<run_length_bits;
@@ -93,14 +96,11 @@ static int kate_rle_encode_line_basic(size_t count,const unsigned char *values,s
   return 0;
 }
 
-static int kate_rle_encode_line_basic_startend(size_t count,const unsigned char *values,size_t bits,const unsigned char *previous kate_unused,kate_pack_buffer *kpb)
+static int kate_rle_encode_line_basic_startend(size_t count,const unsigned char *values,size_t bits,unsigned char zero,const unsigned char *previous kate_unused,kate_pack_buffer *kpb)
 {
-  const unsigned char zero=values[0];
   const size_t run_length_bits=KATE_RLE_RUN_LENGTH_BITS_BASIC_STARTEND;
   const size_t run_length_cutoff=1<<run_length_bits;
   size_t run_length,run_length_start,run_length_end,max_run_length;
-
-  kate_pack_write(kpb,zero,bits);
 
   max_run_length=(1<<KATE_RLE_RUN_LENGTH_BITS_BASIC_STARTEND_START)-1;
   run_length_start=get_run_length_zero(max_run_length,count,values,zero);
@@ -125,14 +125,11 @@ static int kate_rle_encode_line_basic_startend(size_t count,const unsigned char 
   return 0;
 }
 
-static int kate_rle_encode_line_basic_stop(size_t count,const unsigned char *values,size_t bits,const unsigned char *previous kate_unused,kate_pack_buffer *kpb)
+static int kate_rle_encode_line_basic_stop(size_t count,const unsigned char *values,size_t bits,unsigned char zero,const unsigned char *previous kate_unused,kate_pack_buffer *kpb)
 {
-  const unsigned char zero=values[0];
   const size_t run_length_bits=KATE_RLE_RUN_LENGTH_BITS_BASIC_STOP;
   const size_t run_length_cutoff=(1<<run_length_bits)-1;
   size_t run_length,run_length_start,max_run_length;
-
-  kate_pack_write(kpb,zero,bits);
 
   max_run_length=(1<<KATE_RLE_RUN_LENGTH_BITS_BASIC_STOP_START)-1;
   run_length_start=get_run_length_zero(max_run_length,count,values,zero);
@@ -159,16 +156,14 @@ static int kate_rle_encode_line_basic_stop(size_t count,const unsigned char *val
   return 0;
 }
 
-static int kate_rle_encode_line_empty(size_t count,const unsigned char *values,size_t bits,const unsigned char *previous kate_unused,kate_pack_buffer *kpb)
+static int kate_rle_encode_line_empty(size_t count,const unsigned char *values,size_t bits kate_unused,unsigned char zero,const unsigned char *previous kate_unused,kate_pack_buffer *kpb kate_unused)
 {
-  const unsigned char zero=values[0];
   size_t run_length=get_run_length_zero(count,count,values,zero);
   if (run_length<count) return -1;
-  kate_pack_write(kpb,zero,bits);
   return 0;
 }
 
-static int kate_rle_encode_line_delta(size_t count,const unsigned char *values,size_t bits,const unsigned char *previous,kate_pack_buffer *kpb)
+static int kate_rle_encode_line_delta(size_t count,const unsigned char *values,size_t bits,unsigned char zero kate_unused,const unsigned char *previous,kate_pack_buffer *kpb)
 {
   const size_t run_length_delta_bits=KATE_RLE_RUN_LENGTH_BITS_DELTA;
   const size_t run_length_delta_cutoff=(1<<run_length_delta_bits);
@@ -206,19 +201,66 @@ static int kate_rle_encode_line_delta(size_t count,const unsigned char *values,s
   return 0;
 }
 
+static int kate_rle_encode_line_delta_stop(size_t count,const unsigned char *values,size_t bits,unsigned char zero,const unsigned char *previous,kate_pack_buffer *kpb)
+{
+  const size_t run_length_delta_bits=KATE_RLE_RUN_LENGTH_BITS_DELTA_STOP;
+  const size_t run_length_delta_cutoff=(1<<run_length_delta_bits);
+  const size_t run_length_basic_bits=KATE_RLE_RUN_LENGTH_BITS_BASIC_IN_DELTA_STOP;
+  const size_t run_length_basic_cutoff=(1<<run_length_basic_bits)-1;
+  size_t max_run_length_delta,max_run_length_basic;
+  size_t run_length_delta,run_length_basic;
+
+  if (!previous) return -1;
+
+  while (count>0) {
+    if (values[0]==zero) {
+      size_t run_length=get_run_length(count,count,values);
+      if (run_length==count) {
+        kate_pack_write(kpb,0,1);
+        kate_pack_write(kpb,0,run_length_basic_bits);
+        break;
+      }
+    }
+
+    max_run_length_delta=run_length_delta_cutoff;
+    run_length_delta=get_run_length_identical(max_run_length_delta,count,values,previous);
+
+    max_run_length_basic=run_length_basic_cutoff;
+    run_length_basic=get_run_length(max_run_length_basic,count,values);
+
+    if (run_length_delta>run_length_basic) {
+      kate_pack_write(kpb,1,1);
+      kate_pack_write(kpb,run_length_delta-1,run_length_delta_bits);
+      values+=run_length_delta;
+      previous+=run_length_delta;
+      count-=run_length_delta;
+    }
+    else {
+      kate_pack_write(kpb,0,1);
+      kate_pack_write(kpb,run_length_basic,run_length_basic_bits);
+      kate_pack_write(kpb,values[0],bits);
+      values+=run_length_basic;
+      previous+=run_length_basic;
+      count-=run_length_basic;
+    }
+  }
+
+  return 0;
+}
+
 #ifdef DEBUG
 int kate_rle_stats[8]={0};
 int kate_rle_stats_overall[8]={0};
 #endif
 
-static int kate_rle_try_encoding(kate_pack_buffer *kpb,int (*encoder)(size_t,const unsigned char*,size_t,const unsigned char*,kate_pack_buffer*),size_t width,const unsigned char *values,size_t bits,const unsigned char *previous,int kpb_type,int type)
+static int kate_rle_try_encoding(kate_pack_buffer *kpb,int (*encoder)(size_t,const unsigned char*,size_t,unsigned char,const unsigned char*,kate_pack_buffer*),size_t width,const unsigned char *values,size_t bits,unsigned char zero,const unsigned char *previous,int kpb_type,int type)
 {
   kate_pack_buffer tmp;
   kate_pack_writeinit(&tmp);
-  if ((*encoder)(width,values,bits,previous,&tmp)==0) {
+  if ((*encoder)(width,values,bits,zero,previous,&tmp)==0) {
     long kpb_size=kate_pack_bits(kpb);
     long tmp_size=kate_pack_bits(&tmp);
-    if (kpb_size==0 || tmp_size<kpb_size) {
+    if (kpb_type<0 || tmp_size<kpb_size) {
       kate_pack_writeclear(kpb);
       memcpy(kpb,&tmp,sizeof(tmp));
       return type;
@@ -228,26 +270,36 @@ static int kate_rle_try_encoding(kate_pack_buffer *kpb,int (*encoder)(size_t,con
   return kpb_type;
 }
 
+static unsigned char get_zero(size_t count,const unsigned char *values)
+{
+  int counts[256],n,best=0;
+  memset(counts,0,sizeof(counts));
+  while (count-->0) ++counts[*values++];
+  for (n=0;n<256;++n) if (counts[n]>counts[best]) best=n;
+  return best;
+}
+
 static int kate_rle_encode_best(size_t width,size_t height,const unsigned char *values,size_t bits,kate_pack_buffer *kpb)
 {
   const unsigned char *previous=NULL;
+  const unsigned char zero=get_zero(width*height,values); //values[0];
 
 #ifdef DEBUG
   memset(kate_rle_stats,0,sizeof(kate_rle_stats));
 #endif
 
+  kate_pack_write(kpb,zero,bits);
   while (height>0) {
-    long best_type=0;
+    int best_type=-1;
     kate_pack_buffer best_buffer;
     kate_pack_writeinit(&best_buffer);
 
-    best_type=kate_rle_try_encoding(&best_buffer,&kate_rle_encode_line_empty,width,values,bits,previous,best_type,KATE_RLE_TYPE_EMPTY);
-
-    best_type=kate_rle_try_encoding(&best_buffer,&kate_rle_encode_line_delta,width,values,bits,previous,best_type,KATE_RLE_TYPE_DELTA);
-
-    best_type=kate_rle_try_encoding(&best_buffer,&kate_rle_encode_line_basic,width,values,bits,previous,best_type,KATE_RLE_TYPE_BASIC);
-    best_type=kate_rle_try_encoding(&best_buffer,&kate_rle_encode_line_basic_startend,width,values,bits,previous,best_type,KATE_RLE_TYPE_BASIC_STARTEND);
-    best_type=kate_rle_try_encoding(&best_buffer,&kate_rle_encode_line_basic_stop,width,values,bits,previous,best_type,KATE_RLE_TYPE_BASIC_STOP);
+    best_type=kate_rle_try_encoding(&best_buffer,&kate_rle_encode_line_empty,width,values,bits,zero,previous,best_type,KATE_RLE_TYPE_EMPTY);
+    best_type=kate_rle_try_encoding(&best_buffer,&kate_rle_encode_line_delta,width,values,bits,zero,previous,best_type,KATE_RLE_TYPE_DELTA);
+    best_type=kate_rle_try_encoding(&best_buffer,&kate_rle_encode_line_basic,width,values,bits,zero,previous,best_type,KATE_RLE_TYPE_BASIC);
+    best_type=kate_rle_try_encoding(&best_buffer,&kate_rle_encode_line_basic_startend,width,values,bits,zero,previous,best_type,KATE_RLE_TYPE_BASIC_STARTEND);
+    best_type=kate_rle_try_encoding(&best_buffer,&kate_rle_encode_line_basic_stop,width,values,bits,zero,previous,best_type,KATE_RLE_TYPE_BASIC_STOP);
+    best_type=kate_rle_try_encoding(&best_buffer,&kate_rle_encode_line_delta_stop,width,values,bits,zero,previous,best_type,KATE_RLE_TYPE_DELTA_STOP);
 
 #ifdef DEBUG
     ++kate_rle_stats[best_type];
@@ -273,10 +325,9 @@ int kate_rle_encode(size_t width,size_t height,const unsigned char *values,size_
   return kate_rle_encode_best(width,height,values,bits,kpb);
 }
 
-static int kate_rle_decode_line_empty(size_t count,unsigned char *values,size_t bits,kate_pack_buffer *kpb)
+static int kate_rle_decode_line_empty(size_t count,unsigned char *values,size_t bits kate_unused,unsigned char zero,kate_pack_buffer *kpb kate_unused)
 {
-  int value=kate_pack_read(kpb,bits);
-  memset(values,value,count);
+  memset(values,zero,count);
   return 0;
 }
 
@@ -298,13 +349,11 @@ static int kate_rle_decode_line_basic(size_t count,unsigned char *values,size_t 
   return 0;
 }
 
-static int kate_rle_decode_line_basic_stop(size_t count,unsigned char *values,size_t bits,kate_pack_buffer *kpb)
+static int kate_rle_decode_line_basic_stop(size_t count,unsigned char *values,size_t bits,unsigned char zero,kate_pack_buffer *kpb)
 {
   const size_t run_length_bits=KATE_RLE_RUN_LENGTH_BITS_BASIC_STOP;
   size_t run_length;
   int value;
-
-  const unsigned char zero=kate_pack_read(kpb,bits);
 
   run_length=kate_pack_read(kpb,KATE_RLE_RUN_LENGTH_BITS_BASIC_STOP_START);
   if (run_length>0) {
@@ -330,13 +379,11 @@ static int kate_rle_decode_line_basic_stop(size_t count,unsigned char *values,si
   return 0;
 }
 
-static int kate_rle_decode_line_basic_startend(size_t count,unsigned char *values,size_t bits,kate_pack_buffer *kpb)
+static int kate_rle_decode_line_basic_startend(size_t count,unsigned char *values,size_t bits,unsigned char zero,kate_pack_buffer *kpb)
 {
   const size_t run_length_bits=KATE_RLE_RUN_LENGTH_BITS_BASIC_STARTEND;
   size_t run_length;
   int value;
-
-  const unsigned char zero=kate_pack_read(kpb,bits);
 
   run_length=kate_pack_read(kpb,KATE_RLE_RUN_LENGTH_BITS_BASIC_STARTEND_START);
   if (run_length>0) {
@@ -396,16 +443,52 @@ static int kate_rle_decode_line_delta(size_t count,unsigned char *values,const u
   return 0;
 }
 
+static int kate_rle_decode_line_delta_stop(size_t count,unsigned char *values,const unsigned char *previous,size_t bits,unsigned char zero,kate_pack_buffer *kpb)
+{
+  const size_t run_length_delta_bits=KATE_RLE_RUN_LENGTH_BITS_DELTA;
+  const size_t run_length_basic_bits=KATE_RLE_RUN_LENGTH_BITS_BASIC_IN_DELTA;
+  int value;
+
+  if (!previous) return KATE_E_BAD_PACKET;
+
+  while (count>0) {
+    int type=kate_pack_read1(kpb);
+    if (type) {
+      size_t run_length=1+kate_pack_read(kpb,run_length_delta_bits);
+      if (run_length==0 || run_length>count) return KATE_E_BAD_PACKET;
+      memcpy(values,previous,run_length);
+      values+=run_length;
+      previous+=run_length;
+      count-=run_length;
+    }
+    else {
+      size_t run_length=kate_pack_read(kpb,run_length_basic_bits);
+      if (run_length==0) {
+        memset(values,zero,count);
+        break;
+      }
+      if (run_length>count) return KATE_E_BAD_PACKET;
+      value=kate_pack_read(kpb,bits);
+      memset(values,value,run_length);
+      values+=run_length;
+      previous+=run_length;
+      count-=run_length;
+    }
+  }
+  return 0;
+}
+
 static int kate_rle_decode_best(size_t width,size_t height,unsigned char *values,size_t bits,kate_pack_buffer *kpb)
 {
   const unsigned char *previous=NULL;
   int ret;
 
+  const unsigned char zero=kate_pack_read(kpb,bits);
   while (height>0) {
     int type=kate_pack_read(kpb,KATE_RLE_TYPE_BITS);
     switch (type) {
       case KATE_RLE_TYPE_EMPTY:
-        ret=kate_rle_decode_line_empty(width,values,bits,kpb);
+        ret=kate_rle_decode_line_empty(width,values,bits,zero,kpb);
         break;
       case KATE_RLE_TYPE_DELTA:
         ret=kate_rle_decode_line_delta(width,values,previous,bits,kpb);
@@ -414,10 +497,13 @@ static int kate_rle_decode_best(size_t width,size_t height,unsigned char *values
         ret=kate_rle_decode_line_basic(width,values,bits,kpb);
         break;
       case KATE_RLE_TYPE_BASIC_STARTEND:
-        ret=kate_rle_decode_line_basic_startend(width,values,bits,kpb);
+        ret=kate_rle_decode_line_basic_startend(width,values,bits,zero,kpb);
         break;
       case KATE_RLE_TYPE_BASIC_STOP:
-        ret=kate_rle_decode_line_basic_stop(width,values,bits,kpb);
+        ret=kate_rle_decode_line_basic_stop(width,values,bits,zero,kpb);
+        break;
+      case KATE_RLE_TYPE_DELTA_STOP:
+        ret=kate_rle_decode_line_delta_stop(width,values,previous,bits,zero,kpb);
         break;
       default:
         ret=KATE_E_BAD_PACKET;
