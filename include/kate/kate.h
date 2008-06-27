25259254d2b16d484e7716099285304b56474407
ogg.k.ogg.k <ogg.k.ogg.k@googlemail.com>
2008-06-27 08:20:07 +0000

    bump version to 0.1.5

--- include/kate/kate.h
+++ include/kate/kate.h
@@ -31,7 +31,7 @@ typedef float kate_float;
 /** @{ */
 #define KATE_VERSION_MAJOR 0             /**< major version number of the libkate API */
 #define KATE_VERSION_MINOR 1             /**< minor version number of the libkate API */
-#define KATE_VERSION_PATCH 4             /**< patch version number of the libkate API */
+#define KATE_VERSION_PATCH 5             /**< patch version number of the libkate API */
 /** @} */
 
 /** \name Bitstream version */
