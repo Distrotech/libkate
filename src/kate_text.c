569c059a77df2ed5e22cb1b1e9a563e8030dab30
ogg.k.ogg.k <ogg.k.ogg.k@googlemail.com>
2008-12-06 18:32:27 +0000

    remove test against 0xfffe and 0xffff, unsure it should be :/

--- src/kate_text.c
+++ src/kate_text.c
@@ -21,7 +21,6 @@ inline int kate_is_valid_code_point(int c)
 {
   /* surrogate range is invalid */
   if (c>=0xd800 && c<=0xdfff) return 0;
-  if (c>=0xfffe && c<=0xffff) return 0;
 
 #ifdef ENABLE_CODE_POINTS_ABOVE_0x10ffff
   return c>=0 && c<=0x7fffffff;
