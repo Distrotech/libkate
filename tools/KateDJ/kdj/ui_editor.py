780b04c69a272f60c27208b27d34617295db558a
ogg.k.ogg.k <ogg.k.ogg.k@googlemail.com>
2009-08-15 23:36:08 +0100

    changes can now be saved

--- tools/KateDJ/kdj/ui_editor.py
+++ tools/KateDJ/kdj/ui_editor.py
@@ -10,10 +10,34 @@ class UIEditor(wx.Dialog):
     pre.Create(parent,wx.ID_ANY,title='Editing Kate streams',pos=(100,100),size=(600,400),style=wx.DEFAULT_DIALOG_STYLE|wx.RESIZE_BORDER)
     self.PostCreate(pre)
 
+    box=wx.BoxSizer(wx.VERTICAL)
+    self.SetSizer(box)
+
     self.notebook=EditorNotebook(self)
+    box.Add(self.notebook,1,wx.EXPAND)
+    self.notebook.Bind(wx.EVT_NOTEBOOK_PAGE_CHANGED,self.OnPageChanged)
     if filename!=None:
       self.addStream(filemame)
 
+    buttons=wx.BoxSizer(wx.HORIZONTAL)
+    box.Add(buttons)
+
+    save=wx.Button(self,wx.ID_SAVE,'Save')
+    save.Bind(wx.EVT_BUTTON,self.OnSave)
+    buttons.Add(save)
+
+    save_all=wx.Button(self,wx.ID_SAVE,'Save all')
+    save_all.Bind(wx.EVT_BUTTON,self.OnSaveAll)
+    buttons.Add(save_all)
+
+    quit=wx.Button(self,wx.ID_OK,'Quit')
+    quit.Bind(wx.EVT_BUTTON,self.OnQuit)
+    quit.SetDefault()
+    buttons.Add(quit)
+
+    self.buffers=[]
+
+    self.Layout()
     self.Show(True)
 
   def addStream(self,filename):
@@ -22,6 +46,7 @@ class UIEditor(wx.Dialog):
      panel.Bind(wx.EVT_ERASE_BACKGROUND,lambda x: x)
      editor=Editor(parent=panel)
      panel.editor=editor
+     panel.buffer=buffer
      sizer=wx.BoxSizer(wx.VERTICAL)
      sizer.Add(editor.window,1,wx.EXPAND)
      panel.SetSizer(sizer)
@@ -30,5 +55,39 @@ class UIEditor(wx.Dialog):
      buffer.addEditor(editor)
      buffer.open(filename)
      self.notebook.AddPage(page=panel,text=buffer.name,select=True)
+     self.buffers.append(buffer)
      editor.setFocus()
+
+  def OnPageChanged(self,event):
+    new=event.GetSelection()
+    window=self.notebook.GetPage(new)
+    self.current_buffer=window.buffer
+    event.Skip()
+
+  def OnSave(self,event):
+    self.current_buffer.save()
+    event.Skip()
+
+  def OnSaveAll(self,event):
+    for buffer in self.buffers:
+      buffer.save()
+    event.Skip()
+
+  def getNumChanged(self):
+    changed=0
+    for buffer in self.buffers:
+      if buffer.hasChanged():
+        changed+=1
+    return changed
+
+  def OnQuit(self,event):
+    changed=self.getNumChanged()
+    quit=True
+    if changed>0:
+      dlg=wx.MessageDialog(self,'There are unsaved changes. Quit anyway ?','Unsaved changes')
+      if dlg.ShowModal()!=wx.ID_OK:
+        quit=False
+      dlg.Destroy()
+    if quit:
+      event.Skip()
 
